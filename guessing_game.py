237a7896ac691ca6ab0b34f25f982a455fe28049
Mudassar Khan <mudassarkhan@Macbook.local>
2026-07-22 19:16:18 +0500

    v1.02

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,23 @@
 from random import randint
 
+
 def main():
     print ("Welcome to the Guessing Game!")
     number = randint(1, 10)
+    won = False
     
-    while True:
+    for i in range(5): # 5 attempts
         guess = input("Enter Your Guess Between 1 and 10: ")
-    
         if int(guess) == number:
-            print ("That's correct, you win.")
+            print ("That's correct, *** You Win ***.")
             print ("Thank you for playing.\nGoodbye!")
-            break
+            won = True
+            return
         else:
-            print ("That is not correct. Please try again")
+                print ("That is not correct. Please try again")
+    
+    if not won:
+        print ("The correct number was {}.\nThank you for plaing.\n*** Game Over ***".format(number))
 
 if __name__ == '__main__':
     main()
